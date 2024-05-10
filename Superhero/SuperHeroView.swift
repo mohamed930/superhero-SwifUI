4892db0ecec53242a7dd478ee021220d55726127
mohamed930 <mohamed@gmail.com>
2024-05-10 18:33:15 +0300

    Refactor name and add Assists to project commit

---
+++ Superhero/SuperHeroView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Superhero
+//
+//  Created by Mohamed Ali on 10/05/2024.
+//
+
+import SwiftUI
+
+struct SuperHeroView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct SuperHeroView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuperHeroView()
+    }
+}
