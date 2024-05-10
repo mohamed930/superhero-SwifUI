0c8f5bb25f9c1355c08395d01bd909fa43b0dfe2
mohamed930 <mohamed@gmail.com>
2024-05-10 18:20:18 +0300

    Initial Commit

---
+++ Superhero/SuperheroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroApp.swift
+//  Superhero
+//
+//  Created by Mohamed Ali on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
